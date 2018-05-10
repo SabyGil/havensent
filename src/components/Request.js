50c0d84bf1a71c106dda811f58eaacf5206b1949
mholliday6611 <mholliday6611@gmail.com>
2018-05-10 17:56:24 -0400

    seperate register form

--- src/components/Request.js
+++ src/components/Request.js
@@ -112,7 +112,8 @@ class Request extends Component {
                             lon={this.props.lon}
                             lat={this.props.lat}
                             selected = {this.props.haven}
-                            googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+                            googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDURnY8-RKhvImIKT552ulVjqHmRKdKkr8
+&v=3.exp&libraries=geometry,drawing,places"
                             loadingElement={<div style={{ height: `100%` }} />}
                             containerElement={<div className='style' />}
                             mapElement={<div style={{ height: `100%` }}
