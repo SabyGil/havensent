a37981a47127275e2b6127ad4987a0aaecb7f268
mholliday6611 <mholliday6611@gmail.com>
2018-05-10 13:21:58 -0400

    pushing to heroku

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = "http://127.0.0.1:8000/"
+const baseURL = "https://havensentservertesting.herokuapp.com/"
 var header = function(){
 	return {headers: {'Authorization': "JWT " + localStorage.getItem("token")}}
 }
